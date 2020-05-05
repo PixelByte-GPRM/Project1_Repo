1e79a20883d6dc36d8842fc224f70baa1910f6e8
PixelByte-GPRM <63216313+PixelByte-GPRM@users.noreply.github.com>
2020-05-05 10:32:56 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var searchTerm= "";
+var title = "";
+$("#button-search").on("click", function(){
+
+searchTerm =$("#gameTitle") .val() .trim(); 
+console.log(searchTerm)  
+searchTerm= searchTerm.replace(/\s+/g,'-').toLowerCase();
+var settings = {
+    
+	"async": true,
+	"crossDomain": true,
+	"url": "https://rawg-video-games-database.p.rapidapi.com/games/"+searchTerm,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+		"x-rapidapi-key": "167c3ce9f6msh8d7bb8fbc6d5f58p16260djsn0fddb96bb0f5"
+	}
+}
+$.ajax(settings).done(function (response) {
+	console.log(response);
+}).then(); 
+});
+
