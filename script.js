797cd397be209ce7888250def7fe091b0de9ec34
Pixel Byte <dylanrubin6@gmail.com>
2020-05-06 11:23:32 -0400

    Cleaned and finished the info section of the Search Engine

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 
 var searchTerm= "";
 var title = "";
+var newImage;
+
+
+
 $("#button-search").on("click", function(){
 
 searchTerm =$("#gameTitle") .val() .trim(); 
 console.log(searchTerm)  
 searchTerm= searchTerm.replace(/\s+/g,'-').toLowerCase();
+searchTerm= searchTerm.replace(":",'').toLowerCase();
+newImage = $("<img>");
+
 var settings = {
     
 	"async": true,
@@ -23,15 +30,39 @@ $.ajax(settings).done(function (response) {
 
 }).then( function(response)
 {
-    var image= response.background_image;
+	$(".game-price").empty();
+	$(".game-genre").empty();
+	$(".image").remove();
+
+	var image= response.background_image;
+	
 $(".game-name").html(response.name);
-$(".game-genre").html(response.genres);
-$(".game-price").html(response.stores[0].store.name);
-$(".game-desc").html(response.description);
-$(".game-date").html(response.released);
+
+$(".game-genre").html("Game Genre(s): | ");
+for(var j=0; j<response.genres.length; j++)
+{
+	$(".game-genre").append(response.genres[j].name+ " | ");
+}
+
+$(".game-price").html("Online Store(s): | ");
+for(var i=0; i<response.stores.length; i++)
+{
+	$(".game-price").append(response.stores[i].store.name + " | ");
+}
+
+newImage.attr("src", image);
+newImage.attr("class", "image");
+newImage.attr("alt", "Image for the game");
+newImage.attr("width", "400px");
+newImage.attr("height", "300px");
+$("#game-image").prepend(newImage);
+
+
+$(".game-date").html("Release Date: "+response.released);
+$(".game-desc").html("Description: "+response.description);
+$(".game-esrb").html("ESRB rating: "+response.esrb_rating.name);
+
 
 
 });
 });
-
-
