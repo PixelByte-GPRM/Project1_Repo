105d169a9947b75a1136fa46fb49758ccdeb3ab2
Doris Castro <dorisgeo12@gmail.com>
2020-05-06 13:12:40 -0400

    changes made to javascript & HTML for video non-working

--- script.js
+++ script.js
@@ -18,8 +18,6 @@ var settings = {
 	}
 }
 $.ajax(settings).done(function (response) {
-    console.log(response);
-
 
 }).then( function(response)
 {
@@ -30,8 +28,45 @@ $(".game-price").html(response.stores[0].store.name);
 $(".game-desc").html(response.description);
 $(".game-date").html(response.released);
 
+  // Load the IFrame Player API code asynchronously.
+//   var tag = document.createElement('script');
+//   tag.src = "https://www.youtube.com/player_api";
+//   var firstScriptTag = document.getElementsByTagName('script')[0];
+//   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  var player;
+  function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+      height: '360',
+      width: '640',
+      videoId: response.clip.video,  
+    });
+    console.log(videoId);
+  }
+
+   // 4. The API will call this function when the video player is ready.
+   function onPlayerReady(event) {
+     event.target.playVideo();
+   }
+
+   // 5. The API calls this function when the player's state changes.
+   //    The function indicates that when playing a video (state=1),
+   //    the player should play for six seconds and then stop.
+   var done = false;
+   function onPlayerStateChange(event) {
+     if (event.data == YT.PlayerState.PLAYING && !done) {
+       setTimeout(stopVideo, 6000);
+       done = true;
+     }
+   }
+   function stopVideo() {
+     player.stopVideo();
+   }
 
 });
 });
+
 
 
